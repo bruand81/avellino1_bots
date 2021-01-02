73309f33bf5edbb687200387362adf94f9ec22af
bruand <andrea.bruno@antaresnet.org>
2021-01-02 16:25:46 +0100

    Ready for production

--- avellino1_bots/settings/production.py
+++ avellino1_bots/settings/production.py
@@ -1,3 +1,8 @@
 import os
+from avellino1_bots.settings.base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS.append("avellino1-coca-bot.herokuapp.com")
 
 SECRET_KEY = os.getenv("SECRET_KEY", "error_token")
